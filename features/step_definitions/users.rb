e15b674358b20a729fc239fb0e9aed61bf3f8e8e
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-09-21 17:55:42 -0300

    mapeando elementos

--- features/step_definitions/users.rb
+++ features/step_definitions/users.rb
@@ -7,13 +7,26 @@ Quando('o usuário acessar a página de cadastro') do
   find('.header_user_info').click
   find('#email_create').set Faker::Internet.email 
   find('#SubmitCreate').click
+  sleep 2
+  scroll_to(find('h1[class="page-heading"]'), align: :top)
   find('#customer_firstname').set Faker::Name.first_name
   find('#customer_lastname').set Faker::Name.last_name
   find('#passwd').set VARIABLE['pass']
-  find('#uniform-newsletter').set(true)
-  find('#optin').set(true)
-  sleep 10
-  
+  find('#uniform-newsletter').set true
+  find('#uniform-optin').set true
+
+  scroll_to(find(:xpath, '(//div[@class="account_creation"])[2]'), align: :top)
+  find("#firstname").set Faker::Name.first_name
+  find("#lastname").set Faker::Name.last_name
+  find("#address1").set Faker::Address.full_address
+  find("#city").set Faker::Address.city
+  find("#uniform-id_state")
+  find("#uniform-id_country")
+  find("#other").set Faker::Lorem.characters(number: 25)
+  find("#phone").set Faker::PhoneNumber.cell_phone_with_country_code
+  find("#phone_mobile").set Faker::PhoneNumber.cell_phone_in_e164
+  find("#alias").set Faker::Address.full_address
+  sleep 5
 end
 
 Quando('preencher todos campos') do
