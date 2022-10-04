7fa7692331f9c5e49a28ae687a33b9de638a4387
Ana <ana.stadelhofer@outlook.com>
2022-10-03 21:17:32 -0300

    finish creat user

--- features/pages/users.rb
+++ features/pages/users.rb
@@ -17,7 +17,6 @@ class UsersPage < SitePrism::Page
     element :zip_code, '#postcode'
     element :other_infos, '#other'
 
-
     def registered_access email
         self.icon_user.click
         self.email_user.set email
@@ -25,12 +24,15 @@ class UsersPage < SitePrism::Page
         sleep 2
     end
 
-    def personal_info firstname, lastname, termos
+    def personal_info firstname, lastname, termos, day, month, year
         page.choose("Mrs.")
         self.firstname_user.set firstname
         self.lastname_user.set lastname
         self.password.set VARIABLE['pass']
         self.termos_one.set termos
+        find(:xpath, "//select[@id='days']/option[@value='#{day}']").click
+        find(:xpath, "//select[@id='months']/option[@value='#{month}']").click
+        find(:xpath, "//select[@id='years']/option[@value='#{year}']").click
         self.termos_two.set termos
     end
 
@@ -42,6 +44,7 @@ class UsersPage < SitePrism::Page
         self.other_infos.set other
         self.phone.set phone
         self.phone_mobile.set phone_cell
+        find(:xpath, '//select[@id="id_state"]/option[contains(text(), "Alaska")]').click
         self.zip_code.set zip_code
     end
 
--- features/step_definitions/users.rb
+++ features/step_definitions/users.rb
@@ -1,4 +1,3 @@
-
 Dado('que todos dados de usuário foram gerados') do
   @firstname = Faker::Name.first_name
   @lastname = Faker::Name.last_name
@@ -9,18 +8,20 @@ Dado('que todos dados de usuário foram gerados') do
   @other = Faker::Lorem.characters(number: 25)
   @phone = Faker::PhoneNumber.cell_phone_in_e164
   @phone_cell = Faker::PhoneNumber.cell_phone_with_country_code
-  @zip_code = Faker::Address.zip_code
+  @zip_code = Faker::Address.zip_code(state_abbreviation: 'CO')
+  
+  @day = Faker::Number.between(from: 1, to: 30)
+  @month = Faker::Number.between(from: 1, to: 12)
+  @year = Faker::Number.between(from: 1900, to: 2022)
 end
 
 Quando('o usuário preencher todos campos do cadastro') do
   @users.registered_access @email
-  @users.personal_info @firstname, @lastname, true
+  @users.personal_info @firstname, @lastname, true, @day, @month, @year
   @users.address_info @firstname, @lastname, @address, @city, @other, @phone, @phone_cell, @zip_code
-
-  #find("#days", visible: false).select_option '5'
   find("#submitAccount").click
 end
 
 Então('irá exibir a mensagem {string}') do | message |
-
+  expect(page).to have_content('Welcome to your account. Here you can manage all of your personal information and orders.')
 end
